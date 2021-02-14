e26666aaaf74afec4210ec85d43fa3aa636b4f99
shadmansakib018 <shadmansakib018@gmail.com>
2021-02-14 22:15:06 +0600

    database con

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl : true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+  }
+
   }
 });
 
